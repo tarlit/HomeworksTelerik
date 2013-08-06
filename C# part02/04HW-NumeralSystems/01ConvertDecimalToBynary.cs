257f303783d0ba5c5714877c01921dd23db830b9
tarlit <tarlitan@gmail.com>
2013-08-06 20:50:05 +0300

    Hello
    My HW

---
+++ C# part02/04HW-NumeralSystems/01ConvertDecimalToBynary.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Text;
+//01.Write a program to convert decimal numbers to their binary representation.
+
+class ConvertDecimalToBynary
+{
+    public static StringBuilder IntDecimalToBinary(int number)
+    {
+        StringBuilder sb = new StringBuilder();
+
+        if (number > 0)
+        {
+            sb = sb.Append(0);
+            int pos = 30;
+            while (pos != -1)
+            {
+                if ((number & (1 << pos)) != 0)
+                {
+                    sb = sb.Append(1);
+                }
+                else
+                {
+                    sb = sb.Append(0);
+                }
+                pos--;
+            }
+        }
+        else if (number < 0)
+        {
+            sb = sb.Append(1);
+            number = int.MaxValue - Math.Abs(number + 1);
+            int pos = 30;
+            while (pos != -1)
+            {
+                if ((number & (1 << pos)) != 0)
+                {
+                    sb = sb.Append(1);
+                }
+                else
+                {
+                    sb = sb.Append(0);
+                }
+                pos--;
+            }
+        }
+        else
+        {
+            sb = sb.Append(0);
+        }
+        return sb;
+    }
+
+    static void Main()
+    {
+        Console.Write("Enter an integer : ");
+        int number = int.Parse(Console.ReadLine());
+
+        StringBuilder result = IntDecimalToBinary(number);
+        Console.WriteLine("{0}(10)  ->  {1}(2)", number, result);
+    }
+}
---
+++ C# part02/04HW-NumeralSystems/02ConvertBynaryToDecimal.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+//02.Write a program to convert binary numbers to their decimal representation.
+
+class ConvertBynaryToDecimal
+{
+    public static int BinaryToDecimal(string binaryNumber)
+    {
+        int decNumber = 0;
+        for (int i = 0; i < binaryNumber.Length; i++)
+        {
+            // we start with the least significant digit
+            if (binaryNumber[binaryNumber.Length - i - 1] == '0')
+            {
+                continue;
+            }
+            decNumber += (int)Math.Pow(2, i);            
+        }
+        return decNumber;
+    }
+    
+    static void Main()
+    {
+        Console.WriteLine("Enter a binary number : ");
+        string binaryNumber = Console.ReadLine();
+
+        int number = BinaryToDecimal(binaryNumber);
+        Console.WriteLine("{0}(2) -> {1}(10)", binaryNumber, number);
+    }
+}
